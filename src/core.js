69e9742bf84aaa36b031a13f10eeb3c3f927e02e
zafar-saleem <zafarsaleem3@gmail.com>
2014-05-04 20:55:44 +0200

    Updated .jshintrc file

--- src/core.js
+++ src/core.js
@@ -1,3 +1,7 @@
+// This declares to JSHint that $ is a global variable, 
+// and the false indicates that it should not be overridden
+/*global $:false, jQuery:false */
+
 /*
  * @desc CORE module/Object that consists of core functions
  * @author Zafar Saleem
