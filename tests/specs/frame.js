9f292efeefa9a5a0b5a4196792876a12f3926766
zafar-saleem <zafarsaleem3@gmail.com>
2014-05-21 17:55:01 +0200

    rename CORE to FRAME in tests

--- tests/specs/frame.js
+++ tests/specs/frame.js
@@ -1,50 +1,50 @@
-describe('frame', function () {
+describe('FRAME', function () {
 	describe('Has modulesData object', function () {
 		it('should have modulesData', function () {
-			expect(frame.modulesData).toEqual(jasmine.any(Object));
+			expect(FRAME.modulesData).toEqual(jasmine.any(Object));
 		});
 	});
 
 	describe('Have register method', function () {
-		it('frame should have register() method', function () {
-			expect(frame.register).toEqual(jasmine.any(Function));
+		it('FRAME should have register() method', function () {
+			expect(FRAME.register).toEqual(jasmine.any(Function));
 		});
 
 		it('register() should accept 2 arguments', function () {
-			spyOn(frame, 'register');
-			frame.register('moduleName', {});
-			expect(frame.register).toHaveBeenCalledWith('moduleName', {});
+			spyOn(FRAME, 'register');
+			FRAME.register('moduleName', {});
+			expect(FRAME.register).toHaveBeenCalledWith('moduleName', {});
 		});
 
 		it('register() should accept string as first and Object as second argument', function () {
-			spyOn(frame, 'register');
-			frame.register('moduleName', {});
-			expect(frame.register).toHaveBeenCalledWith(jasmine.any(String), jasmine.any(Object));
+			spyOn(FRAME, 'register');
+			FRAME.register('moduleName', {});
+			expect(FRAME.register).toHaveBeenCalledWith(jasmine.any(String), jasmine.any(Object));
 		});
 	});
 
-	describe('frame has startAll() method', function () {
-		it('frame should have startAll() method', function () {
-			expect(frame.startAll).toEqual(jasmine.any(Function));
+	describe('FRAME has startAll() method', function () {
+		it('FRAME should have startAll() method', function () {
+			expect(FRAME.startAll).toEqual(jasmine.any(Function));
 		});
 
 		it('startAll() should accept 1 argument as Object', function () {
 			var obj = {};
-			spyOn(frame, 'startAll');
-			frame.startAll(obj);
-			expect(frame.startAll).toHaveBeenCalledWith(obj);
+			spyOn(FRAME, 'startAll');
+			FRAME.startAll(obj);
+			expect(FRAME.startAll).toHaveBeenCalledWith(obj);
 		});
 	});
 
 	describe('Method for _isMethod() function', function () {
-		it('frame should have _isMethod() function', function () {
-			expect(frame._isMethod).toEqual(jasmine.any(Function));
+		it('FRAME should have _isMethod() function', function () {
+			expect(FRAME._isMethod).toEqual(jasmine.any(Function));
 		});
 
 		it('_isMethod should accept 1 argument', function () {
-			spyOn(frame, '_isMethod');
-			frame._isMethod('callback');
-			expect(frame._isMethod).toHaveBeenCalledWith('callback');
+			spyOn(FRAME, '_isMethod');
+			FRAME._isMethod('callback');
+			expect(FRAME._isMethod).toHaveBeenCalledWith('callback');
 		});
 	});
 });
